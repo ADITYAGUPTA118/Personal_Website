723329095dcf8943704d2ecfce6d50c5949a2f95
ADITYA <adityagupta200317@gmail.com>
2023-03-09 22:14:13 +0530

    third commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app=express();
+
+app.get('/',function(req,res){
+     res.sendFile(__dirname + '/home.html');
+});
+app.get('/home.html',function(req,res){
+    res.sendFile(__dirname + '/home.html');
+});
+app.get('/achievements.html',function(req,res){
+    res.sendFile(__dirname + '/achievements.html');
+});
+
+app.get('/projects.html',function(req,res){
+    res.sendFile(__dirname + '/projects.html');
+});
+
+app.get('/reachout.html',function(req,res){
+    res.sendFile(__dirname + '/reachout.html');
+});
+app.get('/aboutme.html',function(req,res){
+    res.sendFile(__dirname + '/aboutme.html');
+});
+
+
+app.listen(3000, function () {
+     console.log('Server started on port 3000');
+});
